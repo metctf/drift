75d68354cf8eba561617f40a0a3f66858c0014fa
Zeos-ctrl <astrilcodex@gmail.com>
2023-04-01 17:43:27 +0000

    updated 404 page

--- src/pages/notfound.rs
+++ src/pages/notfound.rs
@@ -3,6 +3,8 @@ use yew::prelude::*;
 #[function_component]
 pub fn NotFound() -> Html {
     html!{
-        <h1>{"404"}</h1>
+        <div class={classes!("not-found")}>
+            <h1>{"O you don't have the right, o you don't have the right, o you don't have the right."}</h1>
+        </div>
         }
 }
